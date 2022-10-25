c1b9eaf578b9609974722b0de0373c2f92104f9a
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-10-25 17:39:03 +0600

    category send with id

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ const port = 5000;
 app.use(cors());
 
 const allCourse = require("./data/course.json");
+const allCategory = require("./data/category.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/category", (req, res) => {
+  res.send(allCategory);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCategory = allCourse.find(
+    (course) => course.category_id === id
+  );
+  res.send(selectedCategory);
 });
 
 app.get("/course", (req, res) => {
