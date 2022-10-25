e808ce48a28a683b368cc28e6beac398f75e7ec4
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-10-25 15:19:42 +0600

    data ready

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+var cors = require("cors");
+const port = 5000;
+
+app.use(cors());
+
+const allCourse = require("./data/course.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/course", (req, res) => {
+  res.send(allCourse);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on ${port}`);
+  console.log("hi", port);
 });
